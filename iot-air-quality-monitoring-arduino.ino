b6010742a7a317cdab033f2aafe873f4eba31ea4
ofojichigozie <ofojichigozie@gmail.com>
2021-06-23 08:40:18 +0100

    Seperated sensor handlers into functions

--- iot-air-quality-monitoring-arduino.ino
+++ iot-air-quality-monitoring-arduino.ino
@@ -1,62 +1,44 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include<TinyGPS++.h>
+#include<SoftwareSerial.h>
+#include<ESP8266WiFi.h>
 #include<DHT.h>
-#include <SoftwareSerial.h>
-#include <TinyGPS++.h>
-
-#include <SDS011.h>
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-//Variables for Wi-Fi connection
-#ifndef STASSID
-  #define STASSID "Malibongwe"
-  #define STAPSK  "Hephzibah"
-#endif
+#include<SDS011.h>
 
 #define DHT_TYPE DHT22
 uint8_t DHTPin = D2;
 DHT dht(DHTPin, DHT_TYPE);
 
-float p10, p25;
-int error;
 SDS011 sds011;
+uint8_t sds011Response;
 
-uint8_t MQ5Pin = A0;
+uint8_t MQ9PowerPin = D0;
+uint8_t MQ9Pin = A0;
 
 WiFiClient mClient;
-const char* ssid     = STASSID;
-const char* password = STAPSK;
-const char* server = "192.168.43.7";
+const char* ssid     = "Malibongwe";
+const char* password = "Hephzibah";
+const char* server = "https://iot-air-quality-backend.herokuapp.com";
 
-TinyGPSPlus GPS;
+TinyGPSPlus gps;
 SoftwareSerial gpsSerial(D5, D6); //(Rx, Tx)
 
-float fLatitude, fLatitude, fLatitude, fLatitude
 // Data to send to server
-String temperature = "25";
-String humidity = "80";
-String gasConcentration = "45";
-String pm25 = "10.5";
-String pm10 = "7.5";
-String latitude = "4.5";
-String longitude = "10.2";
+float temperature, humidity, gasConcentration, pm25, pm10,latitude, longitude;
 
 // Counter to determine when data will be sent to the server
 int intervalCounter = 0;
 
 // Function to send captured sensor data to server
-void sendEnvironmentData(String temperature, String humidity, String gasConcentration, String pm25, String pm10, String latitude, String longitude){
+void sendEnvironmentData(float temperature, float humidity, float gasConcentration, float pm25, float pm10, float latitude, float longitude){
 
   String endPoint = "/api/v1/environment-properties/" + 
-                      temperature + "/" + 
-                      humidity + "/" + 
-                      gasConcentration + "/" + 
-                      pm25 + "/" + 
-                      pm10 + "/" + 
-                      latitude + "/" + 
-                      longitude;
+                      String(temperature) + "/" + 
+                      String(humidity) + "/" + 
+                      String(gasConcentration) + "/" + 
+                      String(pm25) + "/" + 
+                      String(pm10) + "/" + 
+                      String(latitude) + "/" + 
+                      String(longitude);
   
   if (mClient.connect(server, 5000))
   { 
@@ -65,14 +47,14 @@ void sendEnvironmentData(String temperature, String humidity, String gasConcentr
     Serial.println(F("Attempting to send captured data to remote server..."));
 
     const String request =  "GET " + endPoint + " HTTP/1.1\r\n" +
-                            "Host: 192.168.43.7\r\n" +
+                            "Host: https://iot-air-quality-backend.herokuapp.com\r\n" +
                             "Connection: close\r\n" +
                             "\r\n";
                             
     mClient.print(request);
     mClient.flush();
 
-    delay(5000);
+    delay(3000);
 
     Serial.println("Response: ");
     
@@ -87,89 +69,96 @@ void sendEnvironmentData(String temperature, String humidity, String gasConcentr
   mClient.stop();
 }
 
+void handlePmSensor(){
+  sds011Response = sds011.read(&pm25, &pm10);
+  if (!sds011Response) {
+    Serial.print(F("P2.5: "));
+    Serial.println(pm25);
+    Serial.print(F("P10:  "));
+    Serial.println(pm10);
+  }else{
+    Serial.println(F("SDS011 Error"));
+  }
+}
+
+void handleDhtSensor(){
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  Serial.print(F("Temperature: "));
+  Serial.println(temperature);
+  Serial.print(F("Humidity: "));
+  Serial.println(humidity);
+}
+
+void handleMQ9Sensor(){
+  int rawValue = analogRead(MQ9Pin);
+  float sensorVoltage = rawValue / 1024 * 5.0;
+  float RsGas = (5.0 - sensorVoltage)/sensorVoltage;
+  gasConcentration = RsGas/3.78;
+  Serial.print(F("Gas concentration: "));
+  Serial.println(gasConcentration);
+}
+
 void setup() {
   // Set up the serial monitor
   Serial.begin(9600);
+
+  //Set up the Wi-Fi module
+  Serial.println();
+  Serial.print(F("Trying to connect to ")); 
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+  delay(1000);
+  Serial.print(F("WiFi connected - IP address: "));
+  Serial.println(WiFi.localIP());
 
   // Set up DHT22 sensor
   pinMode(DHTPin, INPUT);
   dht.begin();
 
   // Set up the PM sensor
-  sds011.begin(D3, D4);
+   sds011.begin(D3, D4);
 
-  pinMode(MQ5Pin, INPUT);
+  // Set up the MQ9 sensor
+  pinMode(MQ9PowerPin, OUTPUT);
+  digitalWrite(MQ9PowerPin, HIGH); // Power MQ9 via D0
+  pinMode(MQ9Pin, INPUT);
 
-  //Set up the GPS module
+  // Set up the GPS module
   gpsSerial.begin(9600);
-
-  //Set up the Wi-Fi module
-  Serial.println();
-  Serial.print(F("Trying to connect to ")); 
-  Serial.println(String(ssid));
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(F("."));
-  }
-
-  Serial.println();
-  delay(1000);
-  Serial.print(F("WiFi connected - IP address: "));
-  Serial.println(WiFi.localIP());
   
 }
 
 void loop() {
-  // Read temperature and humidity from DHT22 sensor
-  temperature = String(dht.readTemperature());
-  humidity = String(dht.readHumidity());
-  Serial.print(F("Temperature: "));
-  Serial.println(temperature);
-  Serial.print(F("Humidity: "));
-  Serial.println(humidity);
+  while (gpsSerial.available() > 0){
+    if (gps.encode(gpsSerial.read())){
+      if (gps.location.isValid()){
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+        Serial.print(F("Latitude: "));
+        Serial.println(latitude);
+        Serial.print(F("Longitude: "));
+        Serial.println(longitude);
 
-  // Read PM2.5 and PM10 from PM sensor
-  error = sds011.read(&p25, &p10);
-  if (!error) {
-    pm25 = String(p25);
-    pm10 = String(p10);
-    Serial.println("P2.5: "+String(p25));
-    Serial.println("P10:  "+String(p10));
-  }else{
-    Serial.println("SDS011 Error:  "+String(error));
-  }
+        handlePmSensor();
+        handleDhtSensor();
+        handleMQ9Sensor();
 
-  // Read gas concentration from MQ5 sensor
-  int MQ5Output = analogRead(MQ5Pin);
-  //TODO: Compute actual gas concentration from MQ5Output
-  gasConcentration = String(MQ5Output);
-  Serial.println("MQ5:  "+gasConcentration);
-  
-  // Read location data from GPS sensor
-  while(gpsSerial.available() > 0){
-    if (GPS.encode(gpsSerial.read())){
-      if(GPS.location.isUpdated()){
-        double lat = GPS.location.lat();
-        double lng = GPS.location.lng();
-        latitude = String(lat);
-        longitude = String(lng));
-        Serial.println("Latitude: "+latitude);
-        Serial.println("Longitude:  "+longitude);
+        if(intervalCounter%200 == 0){
+          if(temperature != 0 && humidity != 0 && gasConcentration != 0 && pm25 != 0 && pm10 != 0 && latitude != 0 && longitude != 0){
+            sendEnvironmentData(temperature, humidity, gasConcentration, pm25, pm10, latitude, longitude);
+            Serial.println(F("........................................................................."));
+          }
+        }
+
+        intervalCounter++;
       }
     }
   }
-  
-  if(intervalCounter%5 == 0){
-    sendEnvironmentData(temperature, humidity, gasConcentration, pm25, pm10, latitude, longitude);
-    Serial.println(F("........................................................................."));
-  }
-
-  Serial.println();
-  
-  delay(5000);
-
-  intervalCounter++;
 }
